b34b26c2a57b27976698457842a37337b3c5b329
Ivan <voropaev2003zen@gmail.com>
2025-04-22 20:36:07 +0300

    Change static url

--- backend/educationplatform/educationplatform/settings.py
+++ backend/educationplatform/educationplatform/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "api/v1/static/"
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
